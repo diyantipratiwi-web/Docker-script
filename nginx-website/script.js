e5cd71e091ea3c90b7a046597ebdaa199a3f8e35
Diyantipratiwi <24031554016@mhs.unesa.ac.id>
2024-12-20 03:36:56 +0700

    Initial commit

---
+++ nginx-website/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+console.log("Selamat Datang di Website Zahradiyan!"); // Menampilkan pesan di konsol untuk debugging atau informasi.
+
+function welcomeUser() {
+    alert("hai! Makasih ya sudah mengunjungi website kami."); // Menampilkan pesan selamat datang saat halaman dimuat.
+}
+
+window.onload = welcomeUser; // Memastikan fungsi dipanggil setelah halaman selesai dimuat.
